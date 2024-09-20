02e7b91c365ac6c2c1425e21b7c7ae9201ce6292
Ederllon <156702239+Ederllon@users.noreply.github.com>
2024-09-19 22:34:55 -0300

    Update win32comclient_python.py

--- win32comclient_python.py
+++ win32comclient_python.py
@@ -1,1 +1,9 @@
 
+import win32com.client
+
+# Inicializar o Word
+word = win32com.client.Dispatch('Word.Application')
+doc = word.Documents.Add()
+doc.Content.Text = "Texto no Word"
+doc.SaveAs('documento.docx')
+word.Quit()
