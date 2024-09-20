a435013deb159c87f07db2659c6f56262be31b6f
Ederllon <156702239+Ederllon@users.noreply.github.com>
2024-09-20 16:21:41 -0300

    Update win32comclient_python.py

--- win32comclient_python.py
+++ win32comclient_python.py
@@ -5,14 +5,24 @@ word = win32com.client.Dispatch("Word.Application")
 word.Visible = True  # Mostra o Word
 
 # Adiciona um novo documento
-documento = word.Documents.Add()
+document = word.Documents.Add()
 
 # Adiciona texto
-parágrafo = documento.Content
-parágrafo.Text = "Olá, Word!"
+addOnDocument = document.Content
+addOnDocument.Text = "texto"
+
+# função de tabela no word
+def tableFunction():
+    table = document.Tables.Add(document.Range(0, 0) , 3, 2)
+    table.Cell(1, 1).Range.Text = "Célula 1"
+    table.Cell(1, 2).Range.Text = "Célula 2"
+    table.Cell(2, 1).Range.Text = "Célula 3"
+    table.Cell(2, 2).Range.Text = "Célula 4"
+
+tableFunction()
 
 # Salva o documento
-documento.SaveAs("C:\\caminho\\para\\seu\\arquivo.docx")
+document.SaveAs("./document.docx")
 
 # Fecha o Word
-word.Quit()
+# word.Quit()
