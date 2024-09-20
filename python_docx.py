a2501aec07d247076da3b15b0515735758530947
Ederllon <156702239+Ederllon@users.noreply.github.com>
2024-09-19 22:33:03 -0300

    Update python_docx.py

--- python_docx.py
+++ python_docx.py
@@ -1,1 +1,8 @@
 
+from docx import Document
+
+# Criar um novo documento
+doc = Document()
+doc.add_heading('Título do Documento', level=1)
+doc.add_paragraph('Este é um parágrafo no documento.')
+doc.save('documento.docx')
