31705437765e6b7fefdf4b91c0bf057d577413ad
Ederllon <156702239+Ederllon@users.noreply.github.com>
2024-10-27 08:26:55 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,23 +1,53 @@
 import win32com.client as initW
+# test --------------------------------------------------------------------
+# print()
+# word = initW.Dispatch("Word.Application")
+# wordOpenCheck = initW.gencache.EnsureDispatch("Word.Application")
+# word.Visible = True
+# wordOpenCheck.Visible = True 
+# localrepo = input(str('Insert repo: '))
+# localvalue = r'{}' .format(localrepo)
+# word.Documents.Open(localvalue)
+# raise SystemExit("TEST FINISHED!")
+# test  --------------------------------------------------------------------
 
 print('Press ENTER to proceed...')
-num_line = int(input("Insert the number line: "))
-num_column = int(input("Insert the number column: "))
+
+q1 = input(str('Arquive exist? [Y/N] ')).upper()
 
 
 
 word = initW.Dispatch("Word.Application")
 wordOpenCheck = initW.gencache.EnsureDispatch("Word.Application")
-word.Visible = True
-wordOpenCheck.Visible = True 
 
-document = word.Documents.Add()
 
-table = document.Tables.Add(document.Range(0, 0), num_line, num_column)
+
+
+
+if 'N'in q1 : 
+    document = word.Documents.Add()
+    num_line = int(input("Insert the number line: "))
+    num_column = int(input("Insert the number column: "))
+    table = document.Tables.Add(document.Range(0, 0), num_line, num_column)    
+
+else:
+    localrepo = input(str('Insert repo: '))
+    localvalue = r'{}' .format(localrepo)
+    document = word.Documents.Open(localvalue)
+    num_line = int(input("Insert the number line: "))
+    num_column = int(input("Insert the number column: "))
+    table = document.Tables.Add(document.Range(0, 0), num_line, num_column)    
+    
+
+
+
 
 for i in range(num_line):
     for j in range(num_column):
         table.Cell(i + 1, j + 1).Range.Text =''
 
+word.Visible = True
+wordOpenCheck.Visible = True 
+table.Borders.Enable = True  
 
-table.Borders.Enable = True 
+
